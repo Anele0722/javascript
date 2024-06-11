2769f5bf547f55658e97cd4d2cf9ad919674a2de
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2024-06-11 12:45:51 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+
+// Declare variables using let
+let name = 'Anele'; // Declares a variable named 'name' and assigns it the string value 'Anele'
+let age = 30; // Declares a variable named 'age' and assigns it the number value 33
+let isStudent = false; // Declares a variable named 'isStudent' and assigns it the boolean value false
+
+// Print variables
+console.log('Name:', name); 
+console.log('Age:', age); 
+console.log('IsStudent:', isStudent); 
