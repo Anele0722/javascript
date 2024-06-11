b4203a6f6d46cda06b191b8169740c6781eac252
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2024-06-11 12:58:31 +0200

    Create Variable Reassignment.js

---
+++ Variable Reassignment.js
@@ -0,0 +1,16 @@
+// Declare and assign the variable 'favoriteColour'
+let favoriteColour = 'blue';
+
+// Print the initial value of favoriteColour
+console.log(favoriteColour); // Outputs: blue
+
+// Function to reassign favoriteColour
+function changeColor(newColor) {
+    favoriteColour = newColor;
+}
+
+// Call the function to change the color
+changeColor('white');
+
+// Print the updated value of favoriteColour
+console.log(favoriteColour); // Outputs: white
