08ad8d7d85a4d8d46ed7cee522fa1990c069cf80
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2024-06-11 13:03:03 +0200

    Create Arithmetic Operations.js

---
+++ Arithmetic Operations.js
@@ -0,0 +1,15 @@
+// Declaring and assigning the variables num1 and num2
+let num1 = 20;
+let num2 = 10;
+
+// Performing arithmetic operations
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// Printing the results of the arithmetic operations
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
